f1f1c6853805e97131557e019dd01778625ad85d
greyfox97 <deepaksamyal80@gmail.com>
2017-12-25 15:03:14 +0530

    Add files via upload

---
+++ quote.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  var previousquote;
+  var temp;
+  var result=$('#quote');
+  $.ajax({
+    url: 'http://quotes.stormconsultancy.co.uk/random.json',
+    method: 'get',
+    dataType: 'json',
+    success: function(data){
+      temp=data.quote;
+      result.html('<blockquote>' + data.quote + '<br>' + '<footer>' + data.author + '</footer>' + '</blockquote>');
+      previousquote=temp;
+    }
+  });
+  $('#nextquote').click(function(){
+    previousquote=temp;
+    var result=$('#quote');
+    $.ajax({
+      url: 'https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?',
+      method: 'get',
+      dataType: 'json',
+      success: function(data){
+        temp=data.quoteText;
+        result.html('<blockquote>' + data.quoteText + '<br>' + '<footer>' + data.quoteAuthor + '</footer>' + '</blockquote>');
+      }
+    });
+  });
+/*  $('#previousquote').click(function(){
+    var result=$('#quote');
+    result.html('<blockquote>' + previousquote + '<br>' + '<footer>' + previousquote + '</footer>' + '</blockquote>');
+  }); */
+});
