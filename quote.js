8906873579e7fd8d5bbaddaa945d0cfa37d6f485
deepak <beb@deepak.com>
2017-12-31 22:41:50 +0530

    weather

--- quote.js
+++ quote.js
@@ -20,8 +20,8 @@ $(document).ready(function(){
       method: 'get',
       dataType: 'json',
       success: function(data){
-        temp=data.quoteText;
-        result.html('<blockquote>' + data.quoteText + '<br>' + '<footer>' + data.quoteAuthor + '</footer>' + '</blockquote>');
+      temp=data.quoteText;
+      result.html('<blockquote>' + data.quoteText + '<br>' + '<footer>' + data.quoteAuthor + '</footer>' + '</blockquote>');
       }
     });
   });
@@ -29,4 +29,16 @@ $(document).ready(function(){
     var result=$('#quote');
     result.html('<blockquote>' + previousquote + '<br>' + '<footer>' + previousquote + '</footer>' + '</blockquote>');
   }); */
+  $('#getcoordinates').click(function getLocation(){
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+    });
+    function showPosition(position) {
+        var x=$('#coordinates');
+        x.innerHTML = "Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude;
+    }
 });
